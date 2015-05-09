b0c39af9abab97ecc8c1fa6fc4200262c97c36c3
igor <igor.ioriatti@hotmail.com>
2015-05-09 22:32:57 +0200

    build

--- public/dist/application.js
+++ public/dist/application.js
@@ -51,6 +51,10 @@ ApplicationConfiguration.registerModule('articles');
 ApplicationConfiguration.registerModule('core');
 'use strict';
 
+// Use applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('customers');
+'use strict';
+
 // Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('users');
 'use strict';
@@ -198,12 +202,51 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Articles',
+	function($scope, Authentication, Articles) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		console.log('Atricles : '+ Articles);
+
+		$scope.alerts = [
+			{icon:'glyphicon glyphicon-qrcode',
+				color:'btn-success',
+				total:'322',
+				description:'alert 1'
+			},
+			{icon:'glyphicon glyphicon-qrcode',
+				color:'btn-success',
+				total:'322',
+				description:'alert 2'
+			},
+			{icon:'glyphicon glyphicon-qrcode',
+				color:'btn-success',
+				total:'322',
+				description:'alert 3'
+			},
+			{icon:'glyphicon glyphicon-qrcode',
+				color:'btn-success',
+				total:'322',
+				description:'alert 4'
+			},
+			{icon:'glyphicon glyphicon-qrcode',
+				color:'btn-success',
+				total:'322',
+				description:'alert 5'
+			},
+			{icon:'glyphicon glyphicon-qrcode',
+				color:'btn-success',
+				total:'322',
+				description:'alert 6'
+			}
+		];
+
+		$scope.find = function() {
+			$scope.articles = Articles.query();
+		};
 	}
 ]);
+
 'use strict';
 
 //Menu service used for managing  menus
@@ -370,6 +413,275 @@ angular.module('core').service('Menus', [
 		this.addMenu('topbar');
 	}
 ]);
+'use strict';
+
+// Configuring the Articles module
+angular.module('customers').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Customers', 'customers', 'dropdown', '/customers(/create)?');
+		Menus.addSubMenuItem('topbar', 'customers', 'List Customers', 'customers');
+		Menus.addSubMenuItem('topbar', 'customers', 'New Customer', 'customers/create');
+	}
+]);
+'use strict';
+
+//Setting up route
+angular.module('customers').config(['$stateProvider',
+	function($stateProvider) {
+		// Customers state routing
+		$stateProvider.
+		state('listCustomers', {
+			url: '/customers',
+			templateUrl: 'modules/customers/views/list-customers.client.view.html'
+		});
+	}
+]);
+
+'use strict';
+
+// Customers controller
+
+var customersApp = angular.module('customers');
+
+customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authentication', 'Customers', '$modal', '$log',
+	function($scope, $stateParams, Authentication, Customers, $modal, $log) {
+		this.authentication = Authentication;
+
+		// Find a list of Customers
+		this.customers = Customers.query();
+
+
+		$scope.animationsEnabled = true;
+
+		$scope.toggleAnimation = function () {
+			$scope.animationsEnabled = !$scope.animationsEnabled;
+		};
+
+		this.modalCreate = function (size) {
+
+			var modalInstance = $modal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'modules/customers/views/create-customer.client.view.html',
+				controller: ["$scope", "$modalInstance", function ($scope, $modalInstance) {
+					$scope.ok = function () {
+						if(this.createCustomerForm.$valid){
+							$modalInstance.close();
+						}
+					};
+
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
+
+				}],
+				size: size
+			});
+
+			modalInstance.result.then(function (selectedItem) {
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
+
+		this.modalUpdate = function (size, selectedCustomer) {
+
+			var modalInstance = $modal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'modules/customers/views/edit-customer.client.view.html',
+				controller: ["$scope", "$modalInstance", "customer", function ($scope, $modalInstance, customer) {
+					$scope.customer = customer;
+
+					$scope.ok = function () {
+
+						if(this.updateCustomerForm.$valid){
+							$modalInstance.close($scope.customer);
+						}
+
+					};
+
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
+
+				}],
+				size: size,
+				resolve: {
+					customer: function () {
+						return selectedCustomer;
+					}
+				}
+			});
+
+			modalInstance.result.then(function (selectedItem) {
+				$scope.selected = selectedItem;
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
+
+		this.remove = function(customer) {
+			//function call(e){
+			//	if(e.preventDefault){
+			//		e.preventDefault();
+			//	}else{
+			//		e.returnValue = false;
+			//	}
+			//	alert("calling");
+			//}
+			if ( customer ) {
+				customer.$remove();
+				for (var i in this.customers) {
+					if (this.customers [i] === customer) {
+						this.customers.splice(i, 1);
+					}
+				}
+			} else {
+				this.customer.$remove(function() {
+				});
+			}
+
+		};
+	}
+]);
+
+customersApp.controller('CustomersCreateController', ['$scope', 'Customers', "Notify",
+	function($scope, Customers, Notify) {
+		// Create new Customer
+		this.create = function() {
+			// Create new Customer object
+			var customer = new Customers ({
+				firstName: this.firstName,
+				surName: this.surName,
+				suburb: this.suburb,
+				country: this.country,
+				industry: this.industry,
+				email: this.email,
+				phone: this.phone,
+				referred: this.referred,
+				channel: this.channel
+			});
+
+			// Redirect after save
+			customer.$save(function(response) {
+				Notify.sendMsg('NewCustomer', {'id': response._id});
+
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+	}
+]);
+
+customersApp .controller('CustomersUpdateController', ['$scope', 'Customers',
+	function($scope, Customers) {
+
+		$scope.channelOptions = [
+			{id:1, item:'facebook'},
+			{id:2, item:'twitter'},
+			{id:3, item:'email'}
+		];
+
+		// Update existing Customer
+		this.update = function(updatedcustomer) {
+			var customer = updatedcustomer;
+
+			customer.$update(function() {
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+	}
+]);
+
+customersApp .controller('CustomersRemoveController', ['$scope', 'Customers',
+	function($scope, Customers) {
+		//// Remove existing Customer
+		this.remove = function(customer, customers) {
+			//function call(e){
+			//	if(e.preventDefault){
+			//		e.preventDefault();
+			//	}else{
+			//		e.returnValue = false;
+			//	}
+			//	alert("calling");
+			//}
+			if ( customer ) {
+				customer.$remove();
+				for (var i in customers) {
+					if (customers [i] === customer) {
+						customers.splice(i, 1);
+					}
+				}
+			} else {
+				this.customer.$remove(function() {
+				});
+			}
+
+		};
+	}
+]);
+
+
+customersApp.directive('customerList', ['Customers', 'Notify', function(Customers, notify){
+	return {
+		restrict: 'E',
+		transclude : true,
+		templateUrl:'modules/customers/views/customer-list-template.html',
+		link : function(scope, element, attrs){
+			//when
+			notify.getMsg('NewCustomer', function(event, data){
+				scope.customersCtrl.customers = Customers.query();
+			});
+		}
+	};
+}]);
+        //
+        //
+		//// Find existing Customer
+		//$scope.findOne = function() {
+		//	$scope.customer = Customers.get({
+		//		customerId: $stateParams.customerId
+		//	});
+		//};
+
+'use strict';
+angular.module('customers')
+	//Customers service used to communicate Customers REST endpoints
+	.factory('Customers', ['$resource',
+		function($resource) {
+			return $resource('customers/:customerId', { customerId: '@_id'
+			}, {
+				update: {
+					method: 'PUT'
+				}
+			});
+		}
+	])
+
+
+	.factory('Notify', ['$rootScope', function($rootScope) {
+
+			var notify = {};
+
+			notify.sendMsg = function(msg, data){
+				data = data || {};
+				$rootScope.$emit(msg, data);
+
+				console.log('message send!');
+			};
+
+			notify.getMsg = function (msg, func, scope){
+				var unbind = $rootScope.$on(msg, func);
+
+				if(scope){
+					scope.on('destroy', unbind);
+				}
+			};
+			return notify;
+		}
+	]);
+
 'use strict';
 
 // Config HTTP Error Handling
